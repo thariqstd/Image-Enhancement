c849c64346a5d2993b8d3a6196c2c7442d8a3cd3
thariqstd <201951082@std.umk.ac.id>
2023-08-02 17:56:11 +0700

    Update 0.4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 from PIL import Image
-from flask import Flask, flash, request, render_template, send_from_directory, send_file
+from flask import Flask, flash, request, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 import matplotlib
 matplotlib.use('Agg')
@@ -32,37 +32,25 @@ def enhancement_img(filename):
 
 # Fungsi untuk melakukan median filtering pada data menggunakan ukuran filter tertentu.
 def median_filter(data, filter_size):
-    # Inisialisasi list sementara untuk menyimpan nilai-nilai dalam filter
     temp = []
-    # Hitung indeks tengah filter
     indexer = filter_size // 2
-    # Inisialisasi array hasil dengan ukuran yang sama seperti data dan diisi dengan nol
     data_final = np.zeros_like(data)
 
-    # Looping untuk setiap baris pada data
     for i in range(len(data)):
-        # Looping untuk setiap kolom pada data
         for j in range(len(data[0])):
-            # Looping untuk setiap elemen dalam filter
             for z in range(filter_size):
-                # Jika indeks di luar batas atas atau bawah data, tambahkan nol ke dalam temp
                 if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
                     for c in range(filter_size):
                         temp.append(0)
                 else:
-                    # Jika indeks di luar batas kiri atau kanan data, tambahkan nol ke dalam temp
                     if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
                         temp.append(0)
                     else:
-                        # Tambahkan nilai data ke dalam temp sesuai dengan posisi filter
                         for k in range(filter_size):
                             temp.append(data[i + z - indexer][j + k - indexer])
 
-            # Urutkan nilai dalam temp dari yang terkecil hingga terbesar
             temp.sort()
-            # Pilih nilai median dari temp dan simpan pada posisi yang sesuai dalam data_final
             data_final[i][j] = temp[len(temp) // 2]
-            # Bersihkan temp untuk digunakan kembali pada iterasi selanjutnya
             temp = []
     return data_final
 
@@ -84,7 +72,6 @@ def histogram_equalization(img):
 
 #Fungsi untuk menampilkan histogram citra dan menyimpannya dalam format gambar PNG.
 def show_image_histogram(image_path, save_path):
-    # Membaca citra dari path yang diberikan
     image = cv2.imread(image_path)
 
     # Memeriksa apakah citra berhasil dibaca atau tidak
@@ -92,18 +79,15 @@ def show_image_histogram(image_path, save_path):
         print("Gambar tidak dapat dibaca. Pastikan path gambar benar dan gambar ada.")
         return
     
-    # Membuat objek figure untuk menampilkan plot histogram dengan ukuran 7x5 inch
     plt.figure(figsize=(7, 5))
 
     # Memeriksa apakah citra merupakan citra warna (BGR) atau grayscale
     if len(image.shape) == 3 and image.shape[2] == 3:
-        # Jika citra warna, hitung histogram untuk masing-masing kanal warna (RGB)
         hist_channels = []
         for i in range(image.shape[2]):
             hist = cv2.calcHist([image], [i], None, [256], [0, 256])
             hist_channels.append(hist)
 
-        # Plot histogram untuk masing-masing kanal warna
         colors = ['red', 'green', 'blue']
         for i, color in enumerate(colors):
             plt.plot(hist_channels[i], color=color)
@@ -115,7 +99,6 @@ def show_image_histogram(image_path, save_path):
         # Jika citra grayscale, hitung histogram untuk citra tersebut
         hist_gray = cv2.calcHist([image], [0], None, [256], [0, 256])
 
-        # Plot histogram untuk citra grayscale
         plt.plot(hist_gray, color='black')
         plt.title('Histogram')
         plt.xlabel('Nilai Pixel')
@@ -146,12 +129,8 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    # Memeriksa apakah permintaan yang diterima adalah POST
     if request.method == 'POST':
-        # Mengambil file gambar dari permintaan POST
         f = request.files.get('file')
-
-        # Mengambil pilihan algoritma dari form
         algorithm = request.form.get('Algorithm')
 
          # Memeriksa apakah ada file gambar yang diunggah
@@ -159,20 +138,12 @@ def predict():
             flash('Please select an image')
             return render_template('index.html')
         
-        # Mengambil path dasar direktori
         basepath = os.path.dirname(__file__)
-        # Memisahkan nama dan ekstensi file
         filename, file_extension = os.path.splitext(secure_filename(f.filename))
-        # Membuat nama unik dengan menggunakan UUID untuk menghindari nama file yang sama
-        unique_filename = f"{filename}_{str(uuid.uuid4())}{file_extension}"
-        # Menggabungkan path untuk menyimpan file gambar yang diunggah
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
-        # Menyimpan file gambar yang diunggah ke path yang ditentukan
         f.save(file_path)
-        # Mendapatkan nama file saja tanpa path
-        file_name = os.path.basename(file_path)
 
-        # Membaca gambar yang diunggah menggunakan OpenCV
+        file_name = os.path.basename(file_path)
         img = cv2.imread(file_path)
 
         # Menghasilkan histogram sebelum proses gambar dan menyimpannya dalam bentuk file gambar PNG
@@ -182,15 +153,11 @@ def predict():
         # Memeriksa algoritma yang dipilih dan memproses gambar sesuai dengan algoritma yang dipilih
         # Apbalia algoritma yang dipilih adalah median filter
         if algorithm == "Med_Filter":
-            # Menghasilkan nama file untuk hasil enhancement dengan sufiks "medfil"
             enhance_fname = f"{filename}_medfil{file_extension}"
             # Memproses gambar dengan algoritma median_filter_rgb dengan ukuran filter 3x3
             enhancement = median_filter_rgb(img, filter_size=3)
-            # Menggabungkan path direktori, nama file enhancement, dan membuat path lengkap
             enhancement_path = os.path.join(basepath, 'images', secure_filename(enhance_fname))
-            # Mendapatkan nama file dari path enhancement
             fname = os.path.basename(enhancement_path)
-            # Menyimpan hasil enhancement ke path yang telah ditentukan
             cv2.imwrite(enhancement_path, enhancement)
 
             # Menggabungkan path direktori basepath, folder 'images', dan nama file enhancement
@@ -200,7 +167,6 @@ def predict():
             # Memperoleh gambar histogram setelah proses enhancement dan menyimpannya di path yang telah ditentukan
             histogram_base64_enhanced = show_image_histogram(enhancement_path, histogram_path_enhanced)
 
-            # Render halaman predict.html dengan menyediakan informasi hasil prediksi
             return render_template('predict.html', file_name=file_name,
                                    enhancement_file=fname,
                                    before_histogram_base64=histogram_base64_original,
@@ -208,15 +174,10 @@ def predict():
         
         #Apabila algoritma yang dipilih adalah histogram equalization
         elif algorithm == "Hist_Equalization":
-            # Membuat nama file enhancement dengan sufiks "_hiseq" (Histogram Equalization)
             enhance_fname = f"{filename}_hiseq{file_extension}"
-            # Melakukan histogram equalization pada gambar menggunakan fungsi histogram_equalization
             enhancement = histogram_equalization(img)
-            # Menggabungkan path direktori basepath, folder 'images', dan nama file enhancement
             enhancement_path = os.path.join(basepath, 'images', secure_filename(enhance_fname))
-            # Mengambil nama file dari enhancement_path
             fname = os.path.basename(enhancement_path)
-            # Menyimpan gambar enhancement ke path yang telah ditentukan
             cv2.imwrite(enhancement_path, enhancement)
 
             # Menggabungkan path direktori basepath, folder 'images', dan nama file enhancement (setelah enhancement)
@@ -226,7 +187,6 @@ def predict():
             # Memperoleh gambar histogram setelah proses enhancement dan menyimpannya di path yang telah ditentukan
             histogram_base64_enhanced = show_image_histogram(enhancement_path, histogram_path_enhanced)
 
-            # Render halaman predict.html dengan menyediakan informasi hasil prediksi
             return render_template('predict.html', file_name=file_name,
                                    enhancement_file=fname,
                                    before_histogram_base64=histogram_base64_original,
